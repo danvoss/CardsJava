520dc99a209fa2a52e964d37dddd771afae03c1f
Dan <danielandrewvoss@gmail.com>
2016-07-12 10:49:46 -0400

    Initial import

---
+++ src/com/dvoss/Card.java
@@ -0,0 +1,56 @@
+package com.dvoss;
+
+/**
+ * Created by Dan on 7/12/16.
+ */
+public class Card {
+    enum Suit {
+        CLUBS,      // <-- essentially these are static final fields, so all caps
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    enum Rank {
+        ACE,
+        TWO,
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+
+    Suit suit;
+    Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Card card = (Card) o;
+
+        if (suit != card.suit) return false;
+        return rank == card.rank;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = suit.hashCode();
+        result = 31 * result + rank.hashCode();
+        return result;
+    }
+}
